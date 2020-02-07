ffb62aef5d21c0fd856cc5eda33f3c53fca96da2
Hamza BENDALI BRAHAM <hbendali@ya.ru>
2020-02-07 14:46:08 +0100

    update

--- SpectraArduinoPLC/spc_ard_plc.cpp
+++ SpectraArduinoPLC/spc_ard_plc.cpp
@@ -32,23 +32,56 @@ void SPC_ARD_PLC::SPC_ARD_PLC(){
 
 }
 
-byte SPC_ARD_PLC::extRead(int i){
-  Wire.requestFrom(extAdr[i], 1, true);
-  extReg[i] = Wire.read();             // receive a byte as character
-  return extReg[i];
-}
-
-void SPC_ARD_PLC::extWrite(int i, int value){
-  Wire.beginTransmission(extAdr[i]);   // transmit to device Adr
-  Wire.write(value);                   // sends value byte  
-  Wire.endTransmission();              // stop transmitting
-}
-
-void SPC_ARD_PLC::extRef(){
-  _extI++;
-  if (_extI >= (sizeof(extAdr) / sizeof(extAdr[0]))){
-    _extI=0;
+void SPC_ARD_PLC::extInit(byte extAddr, byte model){
+  if (model == 1){          // A10_8DI_6RO
+    extWrite(extAddr, MCP23017_IODIRA, 0xff); // all for INPUT
+  	extWrite(extAddr, MCP23017_IODIRB, 0xc0); // D0..D5 as OUTPUT, D6..D7 as OUTPUT  
+  }else if (model == 2){    // A10_8DI_8RO
+    extWrite(extAddr, MCP23017_IODIRA, 0xff); // all for INPUT
+  	extWrite(extAddr, MCP23017_IODIRB, 0x00); // D0..D5 as OUTPUT, D6..D7 as OUTPUT  
+  }else if (model == 3){    // A10_16DI
+    extWrite(extAddr, MCP23017_IODIRA, 0xff); // all for INPUT
+  	extWrite(extAddr, MCP23017_IODIRB, 0xff); // D0..D5 as OUTPUT, D6..D7 as OUTPUT  
+  }else if (model == 4){    // A10_16RO
+    extWrite(extAddr, MCP23017_IODIRA, 0x00); // all for INPUT
+  	extWrite(extAddr, MCP23017_IODIRB, 0x00); // D0..D5 as OUTPUT, D6..D7 as OUTPUT  
   }
-  extRead(_extI);
 }
 
+byte SPC_ARD_PLC::extRead(byte extAddr, byte regAddr){
+  if (regAddr == 'A' || regAddr == 'B'){
+    regAddr = MCP23017_GPIOA + regAddr -'A';
+  }
+	// read the current GPIO output latches
+	Wire.beginTransmission(MCP23017_ADDRESS | extAddr);
+	Wire.write(regAddr);
+	Wire.endTransmission();
+	Wire.requestFrom(MCP23017_ADDRESS | extAddr, 1);
+	return Wire.read();
+}
+
+byte SPC_ARD_PLC::extReadPin(byte extAddr, byte regAddr, byte pin){
+  return (extRead(extAddr, regAddr)>> pin) & 1;
+}
+
+void SPC_ARD_PLC::extWrite(byte extAddr, byte regAddr, byte regValue){
+  if (regAddr == 'A' || regAddr == 'B'){
+    regAddr = MCP23017_GPIOA + regAddr -'A';
+  }
+  // Write the register
+	Wire.beginTransmission(MCP23017_ADDRESS | extAddr);
+	Wire.write(regAddr);
+	Wire.write(regValue);
+	Wire.endTransmission();
+}
+
+void SPC_ARD_PLC::extWritePin(byte extAddr, byte regAddr, byte pin, byte pinValue){
+  if (regAddr == 'A' || regAddr == 'B'){
+    regAddr = MCP23017_OLATA + regAddr -'A';
+  }
+  // read the current GPIO output latches
+	uint8_t regValue = extRead(extAddr, regAddr);
+  // set the value for the particular bit
+	bitWrite(regValue,pin,pinValue);
+  extWrite(extAddr, regAddr, regValue);
+}
--- SpectraArduinoPLC/spc_ard_plc.h
+++ SpectraArduinoPLC/spc_ard_plc.h
@@ -8,6 +8,40 @@
 
 #include "Arduino.h"
 
+#define A10_8DI_6RO 1
+#define A10_8DI_8RO 2
+#define A10_16DI    3
+#define A10_16RO    4
+
+#define MCP23017_ADDRESS 0x20
+// registers
+#define MCP23017_IODIRA 0x00
+#define MCP23017_IPOLA 0x02
+#define MCP23017_GPINTENA 0x04
+#define MCP23017_DEFVALA 0x06
+#define MCP23017_INTCONA 0x08
+#define MCP23017_IOCONA 0x0A
+#define MCP23017_GPPUA 0x0C
+#define MCP23017_INTFA 0x0E
+#define MCP23017_INTCAPA 0x10
+#define MCP23017_GPIOA 0x12
+#define MCP23017_OLATA 0x14
+// -
+#define MCP23017_IODIRB 0x01
+#define MCP23017_IPOLB 0x03
+#define MCP23017_GPINTENB 0x05
+#define MCP23017_DEFVALB 0x07
+#define MCP23017_INTCONB 0x09
+#define MCP23017_IOCONB 0x0B
+#define MCP23017_GPPUB 0x0D
+#define MCP23017_INTFB 0x0F
+#define MCP23017_INTCAPB 0x11
+#define MCP23017_GPIOB 0x13
+#define MCP23017_OLATB 0x15
+// -
+#define MCP23017_INT_ERR 255
+
+
 #define _ON(pin)    digitalWrite(pin, HIGH)
 #define _OFF(pin)   digitalWrite(pin, LOW)
 
@@ -16,14 +50,11 @@ class SPC_ARD_PLC
   public:
     void SPC_ARD_PLC();
     
-    byte extAdr[];
-    byte extReg[];
-    byte extRead(unsigned char i);
-    void extWrite(int i, int value);
-    void extRef();
-  private:
-    byte _extAdr=0;
-    byte _plc=0;
+    void extInit(byte extAddr, byte model);
+    byte extRead(byte extAddr, byte extPort);
+    byte extReadPin(byte extAddr, byte regAddr, byte pin);
+    void extWrite(byte extAddr, byte regAddr, byte regValue);
+    void extWritePin(byte extAddr, byte regAddr, byte pin, byte pinValue);
 };
 
 #endif
